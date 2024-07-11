b8de7488bae3210e329c295eac29b5745d6ef54f
Ivan_Kramoykin <ivankramoykin@gmail.com>
2024-07-11 13:16:32 +0700

    add courses file storage

---
+++ Courses.FileStorage/IFileStorage.cs
@@ -0,0 +1,45 @@
+﻿namespace Courses.FileStorage
+{
+    /// <summary>
+    /// Представляет интерфейс над объектным/файловым хранилищем
+    /// </summary>
+    public interface IFileStorage
+    {
+        ///// <summary>
+        ///// Возвращает сгенерированную ссылку на скачивание файла по имени файла
+        ///// </summary>
+        ///// <param name="filename"></param>
+        ///// <returns> Строка со ссылкой</returns>
+        //public string GetFileLink(string filename);
+
+        ///// <summary>
+        ///// Возвращает кортеж (массив байт, строка), представляющий файл и content-type файла
+        ///// по имени файла
+        ///// </summary>
+        ///// <param name="filename"></param>
+        ///// <returns>Кортеж (массив байт, строка)</returns>
+        //public (byte[] Bytes, string ContentType) GetFile(string filename);
+
+        ///// <summary> проверяет существование файла </summary>
+        ///// <param name="filename"></param>
+        ///// <returns>bool (существует / не существует)</returns>
+        //public bool FileExist(string filename);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="path"></param>
+        public void UploadFile(byte[] file, string path, string fileName);
+
+        //public void DeleteFile(string filename);
+
+        ///// <summary>
+        ///// Возвращает список строк с именами файлов,
+        ///// расположенных по определенному пути в хранилище
+        ///// </summary>
+        ///// <param name="path"></param>
+        ///// <returns>Массив строк</returns>
+        //public List<string> ListFiles(string path);
+    }
+}
---
+++ Courses.FileStorage/LocalStorage.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Courses.FileStorage
+{
+    public class LocalStorage: IFileStorage
+    {
+        protected readonly string _contentPath;
+        //protected readonly IConfiguration configuration;
+
+        public LocalStorage(IConfiguration configuration)
+        {
+            _contentPath = $"{configuration.GetValue<string>("CourseContentDirectoryPath")}";
+        }
+
+        /// <summary>
+        /// Загружает файл в указанную директорию на сервере.
+        /// Если директории нет, создаёт путь до неё целиком.
+        /// Возвращает путь к файлу относительно папки содержимого 
+        /// сайта на сервере.
+        /// </summary>
+        public async void UploadFile(byte[] fileBytes, string path, string fileName)
+        {
+            var fullPath = Path.Combine(_contentPath, path);
+
+            if (!Directory.Exists(fullPath))
+                Directory.CreateDirectory(fullPath);
+
+            string filePath = Path.Combine(_contentPath, path, fileName);
+            if (System.IO.File.Exists(filePath))
+            {
+                throw new IOException($"Файл уже существует: {fileName}");
+            }
+
+            await using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await fileStream.WriteAsync(fileBytes);
+                //await file.CopyToAsync(fileStream);
+            }
+        }
+
+        //public async Task<string> UploadFile(string path, IFormFile file)
+        //{
+        //    var fullPath = Path.Combine(_contentPath, path);
+
+        //    if (!Directory.Exists(fullPath))
+        //        Directory.CreateDirectory(fullPath);
+
+        //    string filePath = Path.Combine(_contentPath, path, file.FileName);
+        //    if (System.IO.File.Exists(filePath))
+        //    {
+        //        throw new IOException($"Файл уже существует: {file.FileName}");
+        //    }
+
+        //    using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+        //    {
+        //        await file.CopyToAsync(fileStream);
+        //    }
+
+        //    return filePath;
+        //}
+    }
+}
