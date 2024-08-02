a4f76cf89bc79287dcb9adb58ab0ab5d9cb594d0
Kramoykin <ivankramoykin@gmail.com>
2024-08-02 15:19:27 +0700

    remove heading name and description length restriction

--- Courses.Model/Labels/Heading.cs
+++ Courses.Model/Labels/Heading.cs
@@ -11,11 +11,9 @@ namespace Courses.Model.Labels
         public int Id { get; set; }
 
         [Required]
-        [StringLength(1)]
         public string Name { get; set; }
 
         [Required]
-        [StringLength(1)]
         public string Description { get; set; }
 
         public List<Label> Labels { get; set; } = new List<Label>();
