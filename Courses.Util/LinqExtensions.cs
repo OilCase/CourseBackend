c6646c870c24ba6503b177924c67ba2d3ada3178
Kramoykin <ivankramoykin@gmail.com>
2024-08-09 14:23:02 +0700

    add sequentional sortings to the heading GET

--- Courses.Util/LinqExtensions.cs
+++ Courses.Util/LinqExtensions.cs
@@ -34,13 +34,40 @@ public static class LinqExtensions
     /// <param name="ascending"></param>
     /// <param name="orderByClause"></param>
     /// <returns></returns>
-    public static IQueryable<T> ParametrizedOrderBy<T>(this IQueryable<T> query, bool? ascending, Expression<Func<T, object>> orderByClause)
+    public static IOrderedQueryable<T> ParametrizedOrderBy<T>(this IQueryable<T> query, bool? ascending, Expression<Func<T, object>> orderByClause)
     {
-        if (ascending == null)
+        if (ascending is null)
+        {
+            return query.OrderBy(x => 0);
+        }
+        if ((bool)ascending)
         {
             return query.OrderBy(orderByClause);
         }
         return query.OrderByDescending(orderByClause);
+    }
+
+    /// <summary>
+    /// Осуществляет дополнительное упорядочивание элементов последовательности
+    /// по возрастанию, если ascending равен true.
+    /// В противном случае сортирует по убыванию
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="query"></param>
+    /// <param name="ascending"></param>
+    /// <param name="orderByClause"></param>
+    /// <returns></returns>
+    public static IOrderedQueryable<T> ParametrizedThenBy<T>(this IOrderedQueryable<T> query, bool? ascending, Expression<Func<T, object>> orderByClause)
+    {
+        if (ascending is null)
+        {
+            return query;
+        }
+        if ((bool)ascending)
+        {
+            return query.ThenBy(orderByClause);
+        }
+        return query.ThenByDescending(orderByClause);
     }
 
     /// <summary>
